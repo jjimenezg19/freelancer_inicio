b78308ff56ec49c683481f4dce76e675f9ec3315
jjimenezg17 <jjimenezg@ucenfotec.ac.cr>
2022-06-02 10:47:32 -0600

    commit inicial

---
+++ freelancer_inicio/codigo.js
@@ -0,0 +1,25 @@
+/** Primer experimento de JavaScript */
+
+var uno = "Hola Mundo ";
+var dos = "Hola mundo como estas ";
+alert(uno);
+
+var pi = 5;
+var numero =99;
+var total = pi + numero;
+alert(total);
+
+var tres, cuatro, cinco, seis;
+
+tres = "chao ";
+cuatro = "No jodan mas ";
+cinco = "Tenga buen dia ";
+seis = "Busque vida ";
+
+document.write(dos + cinco + seis + cuatro + tres);
+
+var a = 40;
+var b = 50;
+
+var resultado = (a>b);
+alert(resultado);
